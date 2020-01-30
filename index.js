ee25e24490db6958c79be3d21f180bb879a270a2
Leonardo S. Matos <matos_leo95@live.com>
2020-01-29 22:26:05 -0300

    Implementa métodos de criar tarefa p/ um projeto, excluir projeto e add middlewares

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ server.use(express.json());
 
 
 const arrProjects = [];
+let countRequests = 0;
+
+//ROTAS
 
 // POST /projects: A rota deve receber id e title dentro do corpo e cadastrar um novo projeto dentro de um array no seguinte formato: { id: "1", title: 'Novo projeto', tasks: [] }; Certifique-se de enviar tanto o ID quanto o título do projeto no formato string com aspas duplas.
 
@@ -18,24 +21,55 @@ const arrProjects = [];
 // POST /projects/:id/tasks: A rota deve receber um campo title e armazenar uma nova tarefa no array de tarefas de um projeto específico escolhido através do id presente nos parâmetros da rota;
 
 
+// Middlewares
+
+// Crie um middleware que será utilizado em todas rotas que recebem o ID do projeto nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não existir retorne um erro, caso contrário permita a requisição continuar normalmente;
+
+// Crie um middleware global chamado em todas requisições que imprime (console.log) uma contagem de quantas requisições foram feitas na aplicação até então;
+
+function checkProjectExists(req, res, next) {
+    const projectIndex = arrProjects.findIndex(elem => elem.id === req.params.id);
+    if(projectIndex < 0){
+        return res.status(400).json({error: 'Project does not exists'});    
+    }
+    
+    req.project = {...arrProjects[projectIndex]};
+    req.projectIndex = projectIndex;
+
+    return next();
+}
+
+server.use((req, res, next) => {
+    countRequests++;
+    console.log(`${countRequests} requests`);
+    return next();
+});
+
 
 server.post('/projects', (req, res) => {
     arrProjects.push(req.body);
-    res.json({message: 'success'});
+    return res.json({message: 'success'});
 });
 
-server.get('/projects', (req, res) => {
-    res.json(arrProjects);
+server.get('/projects', (req, res) => res.json(arrProjects));
+
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+    req.project.title = req.body.title;
+    
+    arrProjects[req.projectIndex] = req.project;
+    return res.json({message: 'Altered with success'});
 });
 
-server.put('/projects/:id', (req, res) => {
-    const project = arrProjects.filter((ele, index) => {
-        if(ele.id === req.params.id){
-            return ele.title = req.body.title;
-        }
-    });
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+    arrProjects.splice(req.projectIndex, 1);
+    return res.json();
+});
 
-    res.json({retorno: project});
+server.post('/projects/:id/task', checkProjectExists, (req, res) => {
+    req.project.tasks.push(req.body.title);
+
+    arrProjects[req.projectIndex] = req.project;
+    return res.json({message: 'Task added'});
 });
 
 server.listen(3000);
